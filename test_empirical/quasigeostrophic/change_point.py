8178ddbf2b5012ac22652446e92cde51b1c5837c
thopri <thopri@noc.ac.uk>
2025-05-12 17:58:31 +0100

    added MBIC function to change point script

--- test_empirical/quasigeostrophic/change_point.py
+++ test_empirical/quasigeostrophic/change_point.py
@@ -1,7 +1,31 @@
 import matplotlib.pyplot as plt
 import ruptures as rpt
 import pandas as pd
+import numpy as np
 
+def calculate_BIC(data, sensitivity, cal=None):
+    if cal is None:
+        cal = [6, 3, 1.5]
+    if sensitivity == "Low":
+        return cal[0] * np.log(len(data))
+    elif sensitivity == "Medium":
+        return cal[1] * np.log(len(data))
+    elif sensitivity == "High":
+        return cal[2] * np.log(len(data))
+
+def calculate_MBIC(data, sensitivity, k, cal=None):
+    if cal is None:
+        cal = [12, 6, 3]
+    if sensitivity == "Low":
+        p = cal[0]
+    elif sensitivity == "Medium":
+        p = cal[1]
+    elif sensitivity == "High":
+        p = cal[2]
+    else:
+        raise Exception("Invalid sensitivity")
+
+    return (p * (k + 1)+k) * np.log(len(data))/2
 
 df = pd.read_csv(
     f"data/qg016.eng.csv",
@@ -9,10 +33,16 @@ df = pd.read_csv(
     names=["Age", "Proxy", ],
 )
 
+signal = df["Proxy"][300:].values
+
+pen = calculate_MBIC(signal, sensitivity="Low",k=5)
+
+print(f"penalty = {pen}")
+
 # detection
-algo = rpt.Pelt(model="rbf").fit(df["Proxy"][300:].values)
-result = algo.predict(pen=25)
+algo = rpt.Pelt(model="l2").fit(signal=signal)
+my_bkps = algo.predict(pen=pen)
 
 # display
-rpt.display(df["Proxy"][300:], result, result)
+rpt.display(signal, my_bkps, my_bkps)
 plt.show()
