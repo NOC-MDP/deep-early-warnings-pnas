ffd9bada1632e8b1fca599a316063e2d44796f3e
Thomas Prime <thopri@host838.jc.rl.ac.uk>
2025-06-12 14:53:06 +0100

    added slurm scripts and even more data

---
+++ test_empirical/NEMO-MEDUSA/slurm/TOS_LAB_500.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=TOS_LAB_500
+#SBATCH --partition=standard
+#SBATCH --account=nemo
+#SBATCH --qos=standard
+#SBATCH --time=24:00:00
+
+/home/users/thopri/micromamba/envs/dew/bin/python ../main.py --parameter TOS --region Labrador_Sea --five_hundred
